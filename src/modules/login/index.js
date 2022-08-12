d493d05b951ccd543df2588c95a51a041addf604
Arbaz5631 <arbaz2279khan@gmail.com>
2022-08-12 17:14:26 +0530

    format the code .

--- src/modules/login/index.js
+++ src/modules/login/index.js
@@ -1,25 +1,26 @@
-import React, {useState} from 'react'
-import Login from "./Login"
-import LoginService from "../../services/login"
-import { useNavigate } from 'react-router'
-import { pathConstants } from '../../constants'
-const UserLogin=()=>{
-    const history=useNavigate()
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const onLogin=async()=>{
-        const loginData= await LoginService.Login({email:email,password:password}) 
-        if(loginData.responseData.validateUser){
-            history(pathConstants.DASHBOARD_MENU.HOME)
-        }
-        else{
-            alert("incorrect id and password")
-        }
-
+import React, { useState } from "react";
+import Login from "./Login";
+import LoginService from "../../services/login";
+import { useNavigate } from "react-router";
+import { pathConstants } from "../../constants";
+const UserLogin = () => {
+  const history = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onLogin = async () => {
+    const loginData = await LoginService.Login({
+      email: email,
+      password: password,
+    });
+    if (loginData.responseData.validateUser) {
+      history(pathConstants.DASHBOARD_MENU.HOME);
+    } else {
+      alert("incorrect id and password");
     }
-    return (
-        <Login onLogin={onLogin} setEmail={setEmail} setPassword={setPassword}/>
-    )
-}
+  };
+  return (
+    <Login onLogin={onLogin} setEmail={setEmail} setPassword={setPassword} />
+  );
+};
 
 export default UserLogin;
